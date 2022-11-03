74687c883a80c470db72bc082122dda163999478
Giomx1 <105117127+Giomx1@users.noreply.github.com>
2022-11-03 15:08:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+/*
+To help you update the logic, 
+  when a user adds a new todo, 
+  the "addItem" function should:
+1. create the "makeObject"
+2. add the object to your todos array
+3. Call the "addToLocalStorage" 
+to the todos are saved in localStorage.
+
+Second thing to do is to create a "loadTodos" 
+function, that checks if todos exist 
+in localStorage, and if they do, 
+to assign that array to your "todos" variable. 
+No need for an "else".
+Call that function to make sure it runs.
+
+With these two, 
+you should be able to 
+add a todo to localStorage now.
+
+Once you have that down, 
+update your "loadTodos" function 
+to have a "for loop" that loops through 
+the array of todos, and displays them on the UI.
+*/
 let todoInput = document.getElementById('todo-input');
 const toDoItemsList = document.getElementById('todo-items');
 let addButton = document.getElementById("add-button")
@@ -6,7 +31,7 @@ let button = document.querySelector("input")
 let itemList = document.createElement("li");
 let deleteButton = document.createElement("button");
 
-button.addEventListener('click', function handleClick(event) {
+button.addEventListener('click', function() {
  let todoInput = document.getElementById('todo-input');
  todoInput.value = "";
 });
@@ -21,6 +46,7 @@ function addItem(){
      toDoItemsList.removeChild(itemList);  
   }
 }
+addButton.addEventListener("click", addItem)
 
 
  itemList.addEventListener("click", exButton)
@@ -29,11 +55,11 @@ function addItem(){
     deleteButton.innerHTML = "X";
     itemList.appendChild(deleteButton)
     itemList.removeEventListener("click", exButton)
-    deleteitem()
+    deleteButton.addEventListener("click", deleteItem)
    
   }
 
- deleteButton.addEventListener("click", deleteItem)
+
     function deleteItem(){
       toDoItemsList.removeChild(itemList);
     } 
@@ -48,4 +74,9 @@ todos.push(makeObject);
   
   function addToLocalStorage(todos) {
   localStorage.setItem('todos', JSON.stringify(todos));
+}
+function loadToDos(){
+  for (let i = 0; i < todos.length; i++) {
+      
+  }
 }
